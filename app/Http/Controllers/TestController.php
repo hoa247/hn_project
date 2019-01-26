ee23291d145bdfbd3c771e2fa5ed2fea3ae36cbd
hoa247 <trandinhhoa.tc@gmail.com>
2019-01-26 09:50:08 +0700

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+class TestController extends Controller
+{
+    public function test()
+    {
+        // 
+    }
+}
