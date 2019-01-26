39a1da06439ebd2c5fc635394376ac4abdca551f
hoa247 <trandinhhoa.tc@gmail.com>
2019-01-26 15:59:14 +0700

    read html category

---
+++ public/js/home.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    var bookContent = $('#amazon-origin-data').find('#merchandised-content');
+    var bookCategories = bookContent.find('.acswidget')
+
+    var categoriesArr=[];
+    bookCategories.each(function(index){
+        var category = {};
+        category.title = $(this).find('.acswidget-carousel__title').text();
+
+        var products = $(this).find('li');
+        var productsArr=[];
+
+        products.each(function(index){
+            var product = {};
+            product.img = $(this).find('img').attr('src');
+            product.link = 'https://www.amazon.com' + $(this).find('a').attr('href');
+            product.name = $(this).find('.acs_product-title').find('.a-size-base').text();
+            product.price = $(this).find('.acs_product-price__buying:first').text();
+
+            var productArr=[];
+            productsArr.push(product); 
+        });
+
+        category.products = productsArr;
+        categoriesArr.push(category); 
+    });
+
+    for (var i = 0; i < categoriesArr.length; i++) {
+        var category = categoriesArr[i];
+        var tmp = $('#clone-category').clone();
+        $(tmp).find('.title-text').text(category.title);
+        $(tmp).find('.hn-products').attr('id', 'hn-products'+i);
+        $('.hn-categories').append(tmp);
+        var products = category.products;
+        for (var j = 0; j < products.length; j++) {
+            product = products[j];
+            if (product.img != "https://images-na.ssl-images-amazon.com/images/G/01/acs/ux/transparent.png") {
+                productTmp = $('#clone-product').clone();
+                $(productTmp).find('.product-name').text(product.name);
+                $(productTmp).find('.price').text(product.price);
+                $(productTmp).find('.product-img').attr('src', product.img);
+                $(productTmp).find('.add-cart').attr('href', "detail?link=" + product.link);
+                $('#hn-products'+i).append(productTmp);
+                $(productTmp).show();
+            }
+        }
+        $(tmp).show();
+    }
+});
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('test', 'TestController@test');
+Route::get('/', 'ProductController@readHTMLDashboard');
